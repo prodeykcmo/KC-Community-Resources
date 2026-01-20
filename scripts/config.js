7ebffc81042e730f0486925017ebbbad7edde01f
prodeykcmo <paige.rodenberg@gmail.com>
2026-01-20 16:28:16 -0600

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 // Paste your published Google Sheets CSV URLs here.
 // File > Share > Publish to web > (choose sheet) > CSV
 const CONFIG = {
-  DIRECTORY_CSV_URL: "",      // "https://docs.google.com/spreadsheets/d/e/2PACX-1vRNdFhK6oO45Dr8IerDN1ZaW-18ccfic9J-eTl-cStqXDeWWWO7CH4FZu5HiYHV2euON0JShYRopfec/pub?gid=1123058370&single=true&output=csv",
+  DIRECTORY_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRNdFhK6oO45Dr8IerDN1ZaW-18ccfic9J-eTl-cStqXDeWWWO7CH4FZu5HiYHV2euON0JShYRopfec/pub?gid=1123058370&single=true&output=csv",
   LANGUAGES_CSV_URL: "",      // optional; if blank, the site uses a built-in language list
   SUBMIT_UPDATE_URL: "",      // Google Form link
   DEFAULT_LANGUAGE_NAME: "English",
